45ef3fe864565781bdb295ebdd3f6986248d9ad0
itstooerli <91441501+itstooerli@users.noreply.github.com>
2022-06-08 14:30:47 -0500

    fixed app console log line

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -23,7 +23,6 @@ class App extends React.Component {
         })
         .then(response => response.json())
         .then(data => {
-            console.log('test');
             this.setState({
                 user: data
             })
