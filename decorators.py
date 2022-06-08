dc3d53ad5d5137fe34c94c0cd6279297cead4940
itstooerli <91441501+itstooerli@users.noreply.github.com>
2022-06-08 14:02:20 -0500

    testing heroku fixes

--- decorators.py
+++ decorators.py
@@ -4,10 +4,9 @@ from flask import redirect
 def jwt_or_login(view_function):
     def wrapper(*args, **kwargs):
         try:
-            flask_jwt_extended.verify_jwt_in_request()
+            print(flask_jwt_extended.verify_jwt_in_request(optional=True))
             return view_function(*args, **kwargs)
         except:
-            print("testing!")
             return redirect('/login', code=302)
             
     # https://stackoverflow.com/questions/17256602/assertionerror-view-function-mapping-is-overwriting-an-existing-endpoint-functi
